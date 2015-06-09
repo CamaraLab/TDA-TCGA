519301e877eb3f9749a52374901227ec620b03fd
udicuty <udi.rubin@gmail.com>
2015-06-09 14:52:45 -0400

    Adding getopt library

--- Udi/env_prep.R
+++ Udi/env_prep.R
@@ -5,5 +5,6 @@ r["CRAN"]="http://cran.uk.r-project.org"
 install.packages('igraph',repos=r)
 install.packages('rgexf',repos=r)
 install.packages('jsonlite',repos=r)
+install.packages('getopt',repos=r)
 
 
