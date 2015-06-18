c5d027692a3352537dbf0523229c372e2b32b2ed
Udi Rubin <uer2102@rabadan7.arcs.cpmc.columbia.edu>
2015-06-18 15:32:01 -0400

    Correcting formula and faster calculation of JSD

--- Udi/divergence.R
+++ Udi/divergence.R
@@ -1,27 +1,55 @@
-#setwd("c:/users/udi/Google Drive/Columbia/LAB/Rabadan/SC-TDA/UDI")
 
-
-KLD<-function(P,Q){
-  
-  #Measures Kullback–Leibler divergence between P and Q
-  
-  KL<-sum(P*log2i(P/Q))
-}
-
-JSD<-function(P,Q){
-  #Measures JSD divergence between P and Q. JSD  =  0.5*KLD(P||M)+0.5*KLD(Q||M). 
-  M<-0.5*(P+Q)
-  jsd<-0.5*KLD(P,M)+0.5*KLD(Q,M)
-  return(jsd)
-}
-
-log2i<-function(p) 
+log2i<-function(p)  {
   # Special function that returns 0  if log2 argument is 0
-  ifelse(log2(p)=="-Inf",0,p)
+  w<-log2(p)
+  ifelse(w=="-Inf",0,w)
+}
 
-JSD1<-function(P,Q)
+#JSD1<-function (P,Q) {
+#  return(sqrt(sum(0.5*P*log2i(P)+0.5*Q*log2i(Q)-0.5*(P+Q)*log2i(0.5*(P+Q)))))
+#}
+
+JSD2<-function(P,Q) {
+  P1<-replace(P,P==0,1) #All Values of 1 will get log2 ==0
+  Q1<-replace(Q,Q==0,1)
+  return(sqrt(sum(0.5*P1*log2(P1)+0.5*Q1*log2(Q1)-0.5*(P+Q)*log2i(0.5*(P+Q))))) 
+}
+
+JSD_matrix<-function (pii_values,){
   
-  0.5*sum(P*log2i(P))+0.5*sum(Q*log2i(Q))-0.5*sum((P+Q)*log2i((P+Q)/2))  
+  
+}
+  
+
+ans<-parLapply(cl,seq_along(pool),function (x) {
+  
+  a1<-matrix(0,d,length(pool[[x]]))
+  
+  for (j in min(pool[[x]]):max(pool[[x]]))
+    for (i in (1:d)) {
+      if (i>=j) {
+        cols<-j-(x-1)*length(pool[[1]])
+        a1[i,cols]<-JSD2(pii1[,i],pii1[,j])
+        #a1[cols,i]<-a1[i,cols]}
+        
+      }
+    }
+  #return(a1)
+  return(a1)
+})
+
+
+
+
+
+
+
+
+P<-pii1[,1]
+Q<-pii1[,2]
+
+
+
 
 #Generating pii_file
 cl <- makeCluster(as.numeric(arg$cores))
@@ -29,11 +57,80 @@ varlist=c("p_connectivity","arg","p_value","permute","c_vector","edges","nodes",
 clusterExport(cl=cl, varlist=varlist,envir=environment())
 
 
+pii1<-as.matrix(fread("/Users/uer2102/Desktop/Merging2/LUAD_Neigh_45_3_TPM.matrix.csv_pii_values.csv",data.table=FALSE))
+pii1<-pii1[,-1]
+pii2<-as.matrix(fread("/Users/uer2102/Desktop/Merging2/LUAD_Neigh_45_3_TPM.matrix.csv-192553-2015-06-17_pii_values.csv",data.table=FALSE))
+pii2<-pii2[,-1]
+identical(round(pii1,4),round(pii2,4))
+identical(pii1,pii2)
 
-pii_val<-read.csv("LUAD_Neigh_45_3_TPM.matrix.csv_pii_values_x.csv",row.names=FALSE)
+pii_val<-as.matrix(fread("/Users/uer2102/Desktop/Merging2/LUAD_Neigh_45_3_TPM.matrix.csv_pii_values.csv",data.table=FALSE))
+pii_val1<-as.matrix(fread("/Users/uer2102/Desktop/Merging2/LUAD_Neigh_45_3_TPM.matrix.csv-192553-2015-06-17_pii_values.csv",data.table=FALSE))
+results<-read.csv("/Users/uer2102/Desktop/Merging2/LUAD_Neigh_45_3_TPM.matrix.csv_results_final-3.csv",row.names=1)
+pii_val<-pii_val[,-1]
+pii_val1<-pii_val1[,-1]
+pii1<-pii[,1]
+
+#View(results)
+#sum(results$pi_fraction==1)
+results<-results[results$pi_fraction!=1,]
+results<-results[results$q_value<=.05,]
+genes_of_interest<-rownames(results)
+pii1<-pii1[,genes_of_interest]
+dim(pii1)
+
+
+
 columns_of_interest<-match(rownames(combined),colnames(pii_val))
-pii<--pii_val[,columns_of_interest]
-pi_val<--pii
+pii<-pii_val[,columns_of_interest]
+
+d<-10
+
+
+a<-matrix(0,d,d)
+
+
+pool<-d
+pool<-clusterSplit(cl,1:d)
+pool
+
+
+#parSapply(cl,pool,function(j) {
+sapply(pool,function(j) {
+  
+  for (i in 1:length(j)) 
+        sapply (j,function (i) { a[i,j]<-JSD1(pii1[,i],pii1[,j]) })
+
+  })
+  
+
+for (1 in min(x):(max(x)-1)) 
+    for (j in (i+1):d) {
+      a[i,j]<-JSD1(pii1[,i],pii1[,j])
+      a[j,i]<-a[i,j]
+      
+  
+})
+
+}
+    
+
+d<-100
+a<-matrix(0,d,d)
+system.time(
+for (i in 1:(d-1)) 
+  for (j in (i+1):d) {
+    a[i,j]<-JSD2(pii1[,i],pii1[,j])
+    a[j,i]<-a[i,j]
+  }
+)
+
+a_slow<-a
+a_fast<-a
+
+identical(a_slow,a_fast)
+a_slow[,1]
+a_fast[,1]
 pii_values<-read.csv("~/Desktop/Merging file/LUAD_Neigh_45_3_TPM.matrix.csv_pii_values.csv",row.names=1)
 combined<-read.csv("~/Desktop/Merging file/combined.csv",row.names=1)
 
@@ -41,7 +138,6 @@ pii_values<-t(pii_values)
 pii_values<-pii_values[rownames(x3),]
 rownames(pii_values)
 dim(pii_values)
-
 
 
 
@@ -88,8 +184,11 @@ for (i in 1:nrow(a))
           a[,y]<-0
 } 
 
-sum(sapply(mylist,function(x) length(x)-1))
+number_of_identical_columns<-sum(sapply(mylist,function(x) length(x)-1))
 
+1000-number_of_identical_columns
+
+sum(sapply(mylist,function(x) if (length(x)-1==0) {return(0)} else return(1)))
 
 
 
