054a170418511da34775409ba7e97cb097434e40
Udi Rubin <uer2102@rabadan7.arcs.cpmc.columbia.edu>
2015-06-18 17:05:42 -0400

    Fixing stuff, writing functions

--- Udi/divergence.R
+++ Udi/divergence.R
@@ -15,9 +15,131 @@ JSD2<-function(P,Q) {
   return(sqrt(sum(0.5*P1*log2(P1)+0.5*Q1*log2(Q1)-0.5*(P+Q)*log2i(0.5*(P+Q))))) 
 }
 
-JSD_matrix<-function (pii_values,){
+columns_cutoff<-function (final_results,q_value_cutoff=.05,min_frac=0,max_frac=1,equal=FALSE){
+ #Recives final_results matrix,q_value,min_frac, and max_frac. And retreives columns of interest
+  #pii_values<-pii1
+  #min_frac<-0
+  #max_frac<-1
+  #q_value_cutoff<-.05
+  #final_results<-results 
+  if (equal) {
+    final_results<-subset(final_results,q_value<=q_value_cutoff)
+    final_results<-subset(final_results,pi_fraction<=max_frac)
+    final_results<-subset(final_results,pi_fraction>=min_frac)  
+  } else{
+    final_results<-subset(final_results,q_value<q_value_cutoff)
+    final_results<-subset(final_results,pi_fraction<max_frac)
+    final_results<-subset(final_results,pi_fraction>min_frac)
+  }
+  
+  genes_of_interest<-rownames(final_results)
+  
+}
+
+JSD_matrix<-function (pii_values,genes_of_interest,cores,d=0,fast=TRUE) {
+  
+  pii_values<-pii_values[,-1] #Subsetting pii matrix
+  if (d==0) d<-length(gene_of_interest)
+  
+  if (!fast) {
+    print ("Building matrix the slow way (no parallel):")
+    mat<-matrix(0,d,d)
+    for (i in 1:(d-1)) 
+        for (j in (i+1):d) {
+          mat[i,j]<-JSD2(pii_values[,i],pii_values[,j])
+          mat[j,i]<-mat[i,j]
+         }
+  } else {
+    
+    print("Building matrix the fast way")
+    print ("Preparing parallel environment")
+    cl <- makeCluster(cores)
+    pool<-clusterSplit(cl,1:d)
+    varlist=c("JSD2","d","pool","pii_values","log2i")
+    clusterExport(cl=cl, varlist=varlist,envir=environment())
+    
+    
+    ans<-parLapply(cl,seq_along(pool),function (x) {
+    #ans<-lapply(seq_along(pool), function(x) {
+      a1<-matrix(0,d,length(pool[[x]])) #Auxilary matrix split big matrix into cores size matrices
+      for (j in min(pool[[x]]):max(pool[[x]]))
+        for (i in (1:d)) {
+          if (i>=j) {
+            cols<-j-(x-1)*length(pool[[1]])
+            a1[i,cols]<-JSD2(pii_values[,i],pii_values[,j])
+            #a1[cols,i]<-a1[i,cols]}
+            
+          }
+        }
+      return(a1)
+    })
+    
+    mat<-NULL
+    for (i in 1:length(ans)) mat<-cbind(mat,ans[[i]])
+    for (i in 1:(d-1))
+      for (j in (i+1):d) mat[i,j]<-mat[j,i]
+  
+  }
+  return(mat)
+}
+results<-read.csv("/Users/uer2102/Desktop/Merging2/LUAD_Neigh_45_3_TPM.matrix.csv_results_final-3.csv",row.names=1)
+genes_of_interest<-columns_cutoff(results,q_value_cutoff=.05,min_frac=0,max_frac=1,equal=FALSE)
+genes_of_interest
+
+pii1<-as.matrix(fread("/Users/uer2102/Desktop/Merging2/LUAD_Neigh_45_3_TPM.matrix.csv_pii_values.csv",data.table=FALSE))
+pii1<-pii1[,-1]
+
+e<-JSD_matrix(pii_values = pii1,genes_of_interest = genes_of_interest,cores = 2,d = 20,fast =FALSE)
+e<-JSD_matrix(pii_values = pii1,genes_of_interest = genes_of_interest,cores = 2,d = 20,fast =TRUE)
+  
+dim(e)
+View(e)
   
   
+  pii1[,1]
+  
+  
+  
+  View(mat)
+  
+  length(ans)
+  ans[[2]]
+  
+  cbind(ans[1:2])
+  mat<-cbind()
+  lapply()
+  for (i in 1:cores) cbind(ans[[1]],ans[[2]])
+  sapply(1:cores,)
+  ans<-cbind(ans[[1]],ans[[2]])
+  
+  #a<-matrix(0,d,d)
+  #for (i in 1:(d-1)) 
+  #  for (j in (i+1):d) {
+  #    a[i,j]<-JSD2(pii_values[,i],pii_values[,j])
+  #    a[j,i]<-a[i,j]
+  #  }
+  
+  
+}
+
+
+
+
+
+
+
+  
+  
+  
+  
+      ?subset
+  
+  colnames(pii_values)
+  pii_values[2,2]
+  dim(pii_values)
+  dim(results)
+  dim(final_results)  
+colnames(final_results)    
 }
   
 
@@ -34,15 +156,14 @@ ans<-parLapply(cl,seq_along(pool),function (x) {
         
       }
     }
-  #return(a1)
   return(a1)
 })
 
 
 
 
-
-
+pii1<-as.matrix(fread("/Users/uer2102/Desktop/Merging2/LUAD_Neigh_45_3_TPM.matrix.csv_pii_values.csv",data.table=FALSE))
+pii1<-pii1[,-1]
 
 
 P<-pii1[,1]
