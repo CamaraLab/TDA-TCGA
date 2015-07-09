65b89cec9732e700771be8bf841249888222a7db
udicuty <udi.rubin@gmail.com>
2015-07-09 17:50:30 -0400

    Adding annotation file to git

---
+++ Udi/annotations.R
@@ -0,0 +1,81 @@
+library(GenomicFeatures)
+
+hg19GeneLengths <- function(symbols)
+{
+  require(TxDb.Hsapiens.UCSC.hg19.knownGene) 
+  require(org.Hs.eg.db)
+  exons.db = exonsBy(TxDb.Hsapiens.UCSC.hg19.knownGene, by='gene')
+  if (symbols=="all") {
+    exon_width<-reduce(exons.db)
+    gene_length<-sum(width(exon_width))
+  } else { 
+           egs    = unlist(  mget(symbols[ symbols %in% keys(org.Hs.egSYMBOL2EG) ],org.Hs.egSYMBOL2EG) )
+           sapply(egs,function(eg)
+           {
+             exons = exons.db[[eg]]
+             exons = reduce(exons)
+             gene_length<-sum( width(exons) )})
+        }
+  return(gene_length)
+}
+symbols<-c("STAT1","CXCL10","ACTB","PDCD1")
+symbols<-"all"
+gene_length<-hg19GeneLengths(symbols)
+gene_length
+
+
+maf<-read.table("../Downloads/PR_TCGA_LUAD_PAIR_Capture_All_Pairs_QCPASS_v4.aggregated.capture.tcga.uuid.automated.somatic.maf",header = TRUE,stringsAsFactors = FALSE)
+
+
+entrez_to_symbol<-as.list(org.Hs.egSYMBOL)
+symbol_to_entrez<-as.list(org.Hs.egALIAS2EG)
+
+
+
+#Replacing unknown entrezids
+require(org.Hs.eg.db)
+xx<-as.list(org.Hs.egALIAS2EG)
+r<-which(maf$Entrez_Gene_Id==0)
+for (i in r) 
+  if (length(xx[[maf$Hugo_Symbol[i]]])==1)
+  {maf$Entrez_Gene_Id[i]<-as.character(xx[maf$Hugo_Symbol[i]])}
+
+maf<-maf[order(maf$Entrez_Gene_Id),]
+head(maf)
+maf<-subset(maf,Entrez_Gene_Id!=0)
+head(maf)
+
+#Replacing old EntrezID's with new ones
+old_new_id<-read.csv("../Google Drive/Columbia/LAB/Rabadan/SC-TDA/Udi/entrez_oldid_conversion.csv")
+for (i in 1:nrow(old_new_id)) 
+maf$Entrez_Gene_Id[maf$Entrez_Gene_Id==old_new_id[i,1]]<-old_new_id[i,2]
+
+maf$Tumor_Sample_Barcode<-substring(maf$Tumor_Sample_Barcode,1,15) #Trim Fix sample name
+maf$Hugo_Symbol<-entrez_to_symbol[maf$Entrez_Gene_Id]
+maf$Synonymous<-(maf$Variant_Classification=="Silent")
+head(maf,30)
+maf$Exons_Length<-gene_length[maf$Entrez_Gene_Id] 
+
+samples<-unique(maf$Tumor_Sample_Barcode)
+genes<-unique(maf$Hugo_Symbol)
+#Check if unique EntrezID!=Unique hugo_symbols
+length(unique(maf$Entrez_Gene_Id))==length(unique(maf$Hugo_Symbol))
+ans<-sapply(symbol,function(x) length(unique(z[z$NAME==as.character(x),1])))
+
+#Creating an empty matrices
+mat_syn<-matrix(0,length(samples),length(unique(maf$Hugo_Symbol)))
+rownames(mat_syn)<-samples
+colnames(mat_syn)<-unique(maf$Entrez_Gene_Id)
+mat_non_syn<-mat_syn
+
+#Creating table of Synonymous mutations for Sample vs Entrez_Gene_Id
+t_syn<-with(maf[maf$Synonymous,],table(Tumor_Sample_Barcode,Entrez_Gene_Id))
+t_non_syn<-with(maf[!maf$Synonymous,],table(Tumor_Sample_Barcode,Entrez_Gene_Id))
+
+#Plugging tables into matrices
+mat_syn[rownames(t_syn),colnames(t_syn)]<-t_syn
+mat_non_syn[rownames(t_non_syn),colnames(t_non_syn)]<-t_non_syn
+colnames(mat_syn)<-entrez_to_symbol[colnames(mat_syn)]
+colnames(mat_non_syn)<-entrez_to_symbol[colnames(mat_non_syn)]
+
+mat_syn["TCGA-17-Z031-01",1:5]
