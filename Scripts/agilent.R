39458ffa5e9da330cbd21c76b502051006988065
Udi Rubin <uer2102@rabadan7.arcs.cpmc.columbia.edu>
2015-07-23 12:05:18 -0400

    Agilent file is rady

--- Scripts/agilent.R
+++ Scripts/agilent.R
@@ -1,28 +1,29 @@
 require(data.table)
 anno_agilent<-read.delim("Google Drive/LAB on Drive/GBM/Agilent/agilent_annotations.txt",stringsAsFactors = F)
 anno_agilent<-select(anno,EntrezGeneID,GeneSymbol) %>% distinct(EntrezGeneID)
-index<-read.delim("Google Drive/LAB on Drive/GBM/Agilent/file_manifest.txt",stringsAsFactors = F,header = T)
-
 files<-list.files("Google Drive/LAB on Drive/GBM/Agilent/Expression/",full.names = T)
-matrix<-fread("Google Drive/LAB on Drive/GBM/Agilent/Expression/US14702406_251584710166_S01_GE2-v5_91_0806.txt_lmean.out.logratio.gene.tcga_level3.data.txt",stringsAsFactors = F,header=FALSE,data.table=F)  
+matrix<-fread("Google Drive/LAB on Drive/GBM/Agilent/Expression/US14702406_251584710166_S01_GE2-v5_91_0806.txt_lmean.out.logratio.gene.tcga_level3.data.txt",stringsAsFactors = F,header=T)  
+samples<-colnames(matrix)[2]
 
-samples<-matrix[1,2]
 for (i in 2:length(files)) {
-  data<-read.delim(files[i],stringsAsFactors = F,header=F)
-  samples<-c(samples,data[1,2])
-  matrix<-merge(matrix,data,by = "V1")
+  data<-fread(files[i],stringsAsFactors = F,header=T)
+  samples<-c(samples,colnames(data)[2])
+  matrix<-merge(matrix,data,by="Hybridization REF")
 }
 
-colnames(matrix)<-samples
-rownames(matrix)<-matrix[,1]
+
+
+
+matrix<-as.matrix(matrix)
+genes<-matrix[,1]
+suppressWarnings(matrix<-apply(matrix,2,function(x) round (as.numeric(x),5)))
+rownames(matrix)<-genes
 matrix<-matrix[,-1]
 matrix<-matrix[sort(rownames(matrix)),sort(colnames(matrix))]
-
-
 rownames(matrix)<-paste0(rownames(matrix),"|",anno$EntrezGeneID[match(rownames(matrix),anno_agilent$GeneSymbol)])
-rownames(matrix)[grepl("NA",rownames(matrix))]
-matrix<-round(matrix,5)
-rownames(matrix)
-getwd()
+matrix<-t(matrix)
+#View(matrix)
+
+
 write.csv(matrix,"Google Drive/LAB on Drive/GBM/Agilent/Agilent.Matrix.csv")
   
