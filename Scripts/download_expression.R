6314d66f102a7102f82548f34a3f0c4b7ef0cc2b
udicuty <udi.rubin@gmail.com>
2015-07-30 17:20:46 -0400

    changed wd

--- Scripts/download_expression.R
+++ Scripts/download_expression.R
@@ -1,9 +1,11 @@
-url<-"https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/gbm/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_GBM.IlluminaHiSeq_RNASeqV2.Level_3.1.2.0/"
+PROJECT_NAME<-"LUSC"
+wd<-paste0("c:/Users/Udi/Documents/TCGA-DATA/",PROJECT_NAME)
+setwd(wd)
+url<-"https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/lusc/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_LUSC.IlluminaHiSeq_RNASeqV2.Level_3.1.9.0/"
 html<-readLines(url)
-files<-html[grep (pattern = "*rsem.genes.results",code)]
+files<-html[grep (pattern = "*rsem.genes.results",html)]
 files<-str_extract(files, ">unc.+\\.rsem.genes.results")
 files<-substring(files,2)
-files[1]
 sapply(files,function(file) {
-  download.file(paste0(url,file),file)
+  download.file(paste0(url,file),paste0("./Expression/",file))
 })
