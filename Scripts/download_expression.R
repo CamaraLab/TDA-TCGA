3380698f582cea057641ab95d5ee24d39c8d261d
udicuty <udi.rubin@gmail.com>
2015-08-25 11:10:58 -0400

    Robust rsem file download tool

--- Scripts/download_expression.R
+++ Scripts/download_expression.R
@@ -1,11 +1,31 @@
-PROJECT_NAME<-"LUSC"
-wd<-paste0("c:/Users/Udi/Documents/TCGA-DATA/",PROJECT_NAME)
-setwd(wd)
-url<-"https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/lusc/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_LUSC.IlluminaHiSeq_RNASeqV2.Level_3.1.9.0/"
+library(stringr)
+library(getopt)
+
+
+spec = matrix(c(
+  "expression", "e",1, "character",
+  # "project", "p", 1, "character",
+  "url", "u",1,"character"
+), byrow=TRUE, ncol=4)
+
+arg<-getopt(spec) #Conmment this line for debug mode
+
+#PROJECT_NAME<-arg$project
+url<-arg$url
+#wd<-paste0(arg$folder,"/",PROJECT_NAME)
+#setwd(wd)
+
+setInternet2(use = TRUE)
+#url<-"https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/skcm/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_SKCM.IlluminaHiSeq_RNASeqV2.Level_3.1.14.0/"
+#PROJECT_NAME<-"SKCM"
 html<-readLines(url)
 files<-html[grep (pattern = "*rsem.genes.results",html)]
 files<-str_extract(files, ">unc.+\\.rsem.genes.results")
 files<-substring(files,2)
+
+dest_dir<-paste0(arg$expression)
+
+
 sapply(files,function(file) {
-  download.file(paste0(url,file),paste0("./Expression/",file))
+  download.file(paste0(url,file),paste0(dest_dir,"/",file))
 })
