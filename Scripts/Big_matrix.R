d952173ccac8482f967b4212a74b7f5c8b697c1c
udicuty <udi.rubin@gmail.com>
2015-08-25 15:21:12 -0400

    Supports command line and 06 sample type

--- Scripts/Big_matrix.R
+++ Scripts/Big_matrix.R
@@ -1,25 +1,49 @@
 require(data.table)
 require(rhdf5)
+require(getopt)
 
-PROJECT_NAME<-"LUSC"
-wd<-paste0("~/TCGA-DATA/",PROJECT_NAME)
-setwd(wd)
+spec = matrix(c(
+  "expression", "e",1, "character",
+  "binary", "b",1, "character",
+  "syn", "s",1, "character",
+  "non_syn", "n",1, "character",
+  "project", "p", 1, "character"
+  
+), byrow=TRUE, ncol=4)
+
+arg<-getopt(spec) #Conmment this line for debug mode
+
+#arg$expression<-"Expression/SKCM_Full_TPM_matrix.csv"
+#arg$binary<-"Mutations/SKCM_Full_Mutations_binary.csv"
+#arg$syn<-"Mutations/SKCM_Full_Mutations_synonymous.csv"
+#arg$non_syn<-"Mutations/SKCM_Full_Mutations_non_synonymous.csv"
+#arg$project<-"SKCM1"
+
+PROJECT_NAME<-arg$project
+#wd<-paste0("~/TCGA-DATA/",PROJECT_NAME)
+#setwd(wd)
 #TPM.matrix<-fread("Expression/GBM_Full_TPM_matrix.csv",data.table = F,header = T)
-TPM.matrix<-fread(paste0("Expression/",PROJECT_NAME,"_Full_TPM_matrix.csv"),data.table = F,header = T)
+#TPM.matrix<-fread(paste0("Expression/",PROJECT_NAME,"_Full_TPM_matrix.csv"),data.table = F,header = T)
+
+print ("Reading matrix files:")
+TPM.matrix<-fread(arg$expression,data.table = F,header = T)
 rownames(TPM.matrix)<-TPM.matrix[,1]; TPM.matrix<-TPM.matrix[,-1]
 
-mat_non_syn_bin<-fread(paste0("Mutations/",PROJECT_NAME,"_Full_Mutations_binary.csv"),data.table = F,header = T)
+#mat_non_syn_bin<-fread(paste0("Mutations/",PROJECT_NAME,"_Full_Mutations_binary.csv"),data.table = F,header = T)
+mat_non_syn_bin<-fread(arg$binary,data.table = F,header = T)
 rownames(mat_non_syn_bin)<-mat_non_syn_bin[,1]; mat_non_syn_bin<-mat_non_syn_bin[,-1]
-mat_non_syn<-fread(paste0("Mutations/",PROJECT_NAME,"_Full_Mutations_non_synonymous.csv"),data.table = F,header = T)
+#mat_non_syn<-fread(paste0("Mutations/",PROJECT_NAME,"_Full_Mutations_non_synonymous.csv"),data.table = F,header = T)
+mat_non_syn<-fread(arg$non_syn,data.table = F,header = T)
 rownames(mat_non_syn)<-mat_non_syn[,1]; mat_non_syn<-mat_non_syn[,-1]
-mat_syn<-fread(paste0("Mutations/",PROJECT_NAME,"_Full_Mutations_synonymous.csv"),data.table = F,header = T)
+#mat_syn<-fread(paste0("Mutations/",PROJECT_NAME,"_Full_Mutations_synonymous.csv"),data.table = F,header = T)
+mat_syn<-fread(arg$syn,data.table = F,header = T)
 rownames(mat_syn)<-mat_syn[,1]; mat_syn<-mat_syn[,-1]
 
 
 
 clean_samples<-function(matrix) {
   samples<-substring(rownames(matrix),1,15)
-  samples_to_keep_1<-which(sapply(samples, function (x) substring(x,14) %in% c("01","10","11")))
+  samples_to_keep_1<-which(sapply(samples, function (x) substring(x,14) %in% c("01","10","11","06","07")))
   #Removing duplicated records
   duplicated<-samples[duplicated(samples)]
   samples_to_keep_2<-which(!samples %in% duplicated)
@@ -32,12 +56,15 @@ clean_samples<-function(matrix) {
 
 
 
-TPM.matrix<-clean_samples(TPM.matrix) #Removing recurrent tumor and dual samples
+#Removing recurrent tumor and dual samples
+print ("Cleaning matrix files:")
+TPM.matrix<-clean_samples(TPM.matrix) 
 mat_non_syn_bin<-clean_samples(mat_non_syn_bin)
 mat_non_syn<-clean_samples(mat_non_syn)
 mat_syn<-clean_samples(mat_syn) 
 
-
+#Inersecting samples
+print ("Intersecting samples")
 samples_of_interest<-intersect(rownames(TPM.matrix),rownames(mat_non_syn_bin))
 TPM.matrix<-TPM.matrix[samples_of_interest,]
 mat_non_syn_bin<-mat_non_syn_bin[samples_of_interest,]
@@ -47,13 +74,15 @@ mat_syn<-mat_syn[samples_of_interest,]
 colnames(mat_non_syn_bin)<-paste0("mut_",colnames(mat_non_syn_bin))
 colnames(TPM.matrix)<-paste0("exp_",colnames(TPM.matrix))
 
+print ("Writing Big_matrix file")
 
 BIG.matrix<-cbind(TPM.matrix[samples_of_interest,],mat_non_syn_bin[samples_of_interest,])
-dim(BIG.matrix)
+#dim(BIG.matrix)
 #write.csv(BIG.matrix,paste0(PROJECT_NAME,"_BIG_matrix.csv"))
 write.csv(BIG.matrix,paste0(PROJECT_NAME,"_BIG_matrix.csv"))
 
 
+print ("Writing h5 files")
 
 h5file<-paste0(PROJECT_NAME,".h5")
 if (file.exists(h5file)) {file.remove(h5file)}
