dabd7bb2d02d9540ecd735a64da9aa62520083ef
udicuty <udi.rubin@gmail.com>
2015-08-25 11:11:33 -0400

    Path change

--- Scripts/run_script.bat
+++ Scripts/run_script.bat
@@ -2,4 +2,7 @@ Rscript connectivity3.R -n "LUAD_Cor_Neigh_42_3_intersect" -m LUAD.h5 -c all -p 
 
 gene name validation
 chunk size command line
-ignore zero columns	
+ignore zero columns
+
+
+Rscript "..\..\..\Google Drive\Columbia\LAB\Rabadan\TCGA-TDA\Scripts\connectivity3.R" -n "LUAD_Cor_Neigh_42_3_intersect" -m LUAD.h5 -c all -p 1000 -q 4 -l FALSE -f TRUE -k 50 -t 30 -g 100 -s syn
