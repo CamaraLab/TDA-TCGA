a8ae078baa8ddd35ccca92e447e35cc055c5d32b
udicuty <udi.rubin@gmail.com>
2015-10-24 13:55:44 -0400

    Network generator

--- Scripts/network_generator.R
+++ Scripts/network_generator.R
@@ -2,13 +2,26 @@ require(sna)
 require(grid)
 require(ggplot2)
 require(ggnet)
-#net = rgraph(6, mode = "graph", tprob = 0.5)
+
+#rgraph(6, mode = "graph", tprob = 0.5)
+net = read.table("net.csv",sep=",",header=TRUE)
+net<-net[,-1]
+colnames(net)<-rownames(net)<-toupper(letters[1:ncol(net)])
+net  
+
+
+
+
 #net = network(net, directed = FALSE)
-label<-colnames(net)<-sample(seq(from = 0, to = 1, by = 0.1), size = 6)
-node.size<-rep(c(20,40),3)
+label<-sample(seq(from = 0, to = 1, by = 0.1), size = 6)
+node.size<-rep(20,6)
+node.color<-c("yellow","red","cyan","green","orange","pink")
 #coord1<-gplot.layout.kamadakawai(net,layout.par=NULL)
-ggnet2(net, node.size = node.size,mode=coord1,label=label,label.color="red")  
-  guides(label=TRUE, size = FALSE)
+ggnet2(net, node.size = node.size,node.color=node.color,mode=coord1,label=label,labels.alpha=1) + geom_text(aes(label=row.names(net)),vjust=-1,alpha=1,fontface="bold",size=10) + 
+  geom_text(aes(label=label),fontface="bold",size=5)
+net
+
+guides(label=TRUE, size = FALSE)
 
 ggnet2(net, size = 10, color = "red",layout.par = list(seed.coord=coord1))
 ggnet2(net, mode = x)
