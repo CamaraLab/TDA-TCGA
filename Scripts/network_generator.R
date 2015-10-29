475e4b9dc87f812c66ed8ec2725b869b512b4782
udicuty <udi.rubin@gmail.com>
2015-10-29 11:57:31 -0400

    sdasd

--- Scripts/network_generator.R
+++ Scripts/network_generator.R
@@ -3,42 +3,42 @@ require(grid)
 require(ggplot2)
 require(ggnet)
 
-#rgraph(6, mode = "graph", tprob = 0.5)
-net = read.table("net.csv",sep=",",header=TRUE)
-net<-net[,-1]
-colnames(net)<-rownames(net)<-toupper(letters[1:ncol(net)])
-net  
+#net<-rgraph(6, mode = "graph", tprob = 0.5)
+#colnames(net)<-rownames(net)<-toupper(letters[1:ncol(net)])
+net = read.table("net1.csv",sep=",",header=TRUE,row.names=1)
+coord1<-as.matrix(read.csv("coord1.csv",row.names=1))
+#write.csv(net,"net.csv")
+#coord1 = gplot.layout.fruchtermanreingold(net, NULL)
+#write.csv(coord1,"coord1.csv")
 
-
-
-
-#net = network(net, directed = FALSE)
-label<-sample(seq(from = 0, to = 1, by = 0.1), size = 6)
-node.size<-rep(20,6)
-node.color<-c("yellow","red","cyan","green","orange","pink")
+mprob<-sample(seq(from = 0, to = 1, by = 0.1), size = 6)
+mprob<-c(100,2000,5,300,100,20)
+node.size<-c(20,20,20,20,20,20)
+node.color<-c("yellow","pink","cyan","green","orange","grey")
 #coord1<-gplot.layout.kamadakawai(net,layout.par=NULL)
-ggnet2(net, node.size = node.size,node.color=node.color,mode=coord1,label=label,labels.alpha=1) + geom_text(aes(label=row.names(net)),vjust=-1,alpha=1,fontface="bold",size=10) + 
-  geom_text(aes(label=label),fontface="bold",size=5)
-net
-
-guides(label=TRUE, size = FALSE)
-
-ggnet2(net, size = 10, color = "red",layout.par = list(seed.coord=coord1))
-ggnet2(net, mode = x)
-
-layout.par<-list(exp=20)
-gplot(net,usearrows = F,displaylabels = T,label.pos=10,vertex.enclose = T)
-gplot.layout.kamadakawai(net, layout.par)
+ggnet2(net, size = node.size,node.color=as.numeric(mprob*2),mode=coord1,label=mprob,label.size=8,legend.size = 12, legend.position = "right") + 
+  geom_text(aes(label=row.names(net)),vjust=-1.2,alpha=1,fontface="bold",size=10)
+  #geom_text(aes(label=letters[1:6]),fontface="bold",size=5)
 
 
-net = network(net, directed = FALSE)
-ggnet(net)
+p_matrix<-matrix(0,6,6)
+for (i in 1:length(mprob)) {
+  for (j in 1:length(mprob)) {
+    p_matrix[i,j]<-net[i,j]*mprob[i]*mprob[j]
+  }
+  
+}
+colnames(p_matrix)<-rownames(p_matrix)<-colnames(net)
+
+write.csv(p_matrix,"p_matrix3.csv")
 
 
-ggnet2(net, color = "phono", palette = "Set1", edge.color = c("color", "grey50"))
-x = gplot.layout.fruchtermanreingold(net, NULL)
-x
+con=0
+for (i in 1:6) {
+  for (j in 1:6) {
+    con=con+net[i,j]*x[i]*x[j]
+  }
+}
+  
+con
 
-ggnet(net, color = "phono")
-devtools::install_github("briatte/ggnet")
-library(ggnet)
