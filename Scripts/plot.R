bc4860bd3485c35547931760dbd1d72b8758b2ab
udicuty <udi.rubin@gmail.com>
2015-11-03 16:27:46 -0500

    Plotting file

--- Scripts/plot.R
+++ Scripts/plot.R
@@ -7,10 +7,11 @@ ggplot(x, aes(x=factor(resolution), y=factor(gain), label=q_0.15,fill=q_0.15)) +
   geom_tile() + theme_minimal() + geom_text() + ggtitle("Genes q<=0.15")
 
 y<-read.csv("scan_summary.csv")
-ggplot(y, aes(x=factor(resolution), y=factor(gain), label=round(mutload,2),color=mutload)) + 
-  scale_color_gradient(low = 'red',high = 'blue',guide_legend(title = "p_value")) +
-  geom_point(size=6) + theme_bw() + geom_text(vjust=1.6) + ggtitle("Mutational load - Colon") + 
-  xlab("Resolution") + ylab ("Gain")
+ggplot(y, aes(x=factor(resolution), y=factor(gain), fill=mutload)) + 
+  scale_fill_continuous(low = 'red',high = 'blue',guide_legend(title = "p_value")) +
+  geom_tile() + theme_minimal() + geom_text(label=round(y$mutload,2),size=7,vjust=-0.1) + ggtitle("Mutational load - Colon") + 
+  xlab("Resolution") + ylab ("Gain") + geom_text(label=paste0("(",y$first_connected_samples,")"),vjust=1,size=6) +
+  coord_equal()
   
 
 qplot(data=x,x=resolution,y=gain,geom="tile",fill=q_0.15,label=q_0.15)
