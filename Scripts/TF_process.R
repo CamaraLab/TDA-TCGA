29359babced42b3513a832fc37bb133df7691c53
udicuty <udi.rubin@gmail.com>
2015-11-16 13:34:19 -0500

    ENCODE

--- Scripts/TF_process.R
+++ Scripts/TF_process.R
@@ -13,16 +13,111 @@ variant_files<-list.files(pattern=".snp.Somatic")
 var_samples<-substring(variant_files,1,12)
 var_samples<-gsub(pattern = "_",replacement = "-",x=var_samples)
 
+
+a<-lapply(variant_files,function (x) {
+  f<-read.table(x,header=TRUE)[,1:2] 
+  f$chrom<-paste0("chr",f$chrom)
+  return(f)
+})
+names(a)<-var_samples
+
+
 TPM<-read.csv("../../LUAD_CUR/Expression/LUAD_Full_TPM_matrix.csv",row.names = 1,as.is=T)
 tpm_samples<-substring(rownames(TPM),1,12)
 samples<-intersect(tpm_samples,var_samples)
 
-
-
-a<-read.table("TCGA_05_4249.snp.Somatic.hc.filtered",header=TRUE)
-noncode<-read.table("../NONCODEv5_hg19_linc.bed")
-noncode<-noncode[,1:4]
+all_chromosomes<-sapply(1:22,function(x) paste0("chr",x))
+noncode<-read.table("../NONCODEv5_hg19_linc.bed",as.is=T)[,1:4]
 colnames(noncode) <- c('chr','start','end','id')
+head(noncode)
+noncode<-subset(noncode,noncode$chr %in% all_chromosomes)
+dim(noncode)
+
+
+for (sample in names(a)[1:5]) {
+  print(sample)
+  var<-a[[sample]]
+  noncode[,sample]<-apply(noncode,1, function (n) {    
+    chr<-n[1]
+    pos_start<-as.numeric(n[2])
+    pos_end<-as.numeric(n[3])
+    ans<-in_range(var$position[var$chrom==chr],pos_start,pos_end)
+    return(ans)
+  })
+}
+
+  
+var<-x
+  noncode$hits<-NA
+  noncode$<-apply(noncode,1, function (n) {    
+    chr<-n[1]
+    pos_start<-as.numeric(n[2])
+    pos_end<-as.numeric(n[3])
+    ans<-in_range(var$position[var$chrom==chr],pos_start,pos_end)
+    return(ans)
+  })
+  return(noncode)
+})
+
+
+
+
+
+noncode$hits<-NA
+noncode$hits<-apply(noncode,1, function (n) {    
+  chr<-n[1]
+  pos_start<-as.numeric(n[2])
+  pos_end<-as.numeric(n[3])
+  ans<-in_range(var$position[var$chrom==chr],pos_start,pos_end)
+  return(ans)
+})
+
+
+  
+  
+  
+  
+  
+  }
+  
+}
+
+
+
+names(samples)<-samples
+t<-lapply(samples[1:2],function (var_file) {
+  var_file<-a[[var_file]]
+  var_file$chrom<-paste0("chr",var_file$chrom)
+  var_file_chr<-split(var_file,var_file$chrom)
+  chromosomes<-names(var_file_chr)
+  names(chromosomes)<-chromosomes
+  noncode_chr<-noncode_chr[chromosomes]
+  
+  hit_matrix_chr<-lapply(chromosomes, function (chr){
+    
+    hit_matrix<-sapply(var_file_chr[[chr]]$position,function (positions) {
+      x<-NULL
+      for (i in 1:nrow(noncode_chr[[chr]])) {
+        x[i]<-in_range(positions,noncode_chr[[chr]][i,"start"],noncode_chr[[chr]][i,"end"])
+      }
+      return(x)
+    })
+    rownames(hit_matrix)<-noncode_chr[[chr]]$id
+    return(hit_matrix)
+  })    
+})
+  
+})
+
+
+hit_matrix<-sapply(a[1:5],function (positions) {
+  x<-NULL
+  for (i in 1:nrow(noncode)) {
+    x[i]<-in_range(positions,noncode[i,"start"],noncode[i,"end"])
+  }
+  return(x)
+})
+rownames(hit_matrix)<-noncode$id
 
 x<-NULL
 for (i in 1:nrow(noncode)) {
